1569957772424de3f54adfcf8537640d4b1b86f6
otherenergy <otherenergy@hotmail.com>
2025-04-24 01:13:37 +0200

    First Changes

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/about-creative":["static\u002Fchunks\u002Fpages\u002Fabout-creative.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fabout-creative"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
